867d41e3178df5e2a7b233e15e4555d95d04923a
suharelli <suharelli@gmail.com>
2023-09-22 15:25:06 +0300

    github link

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,11 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { Box, Button } from "@mui/material";
+import { Box, Button, Link } from "@mui/material";
 import { authSlice } from "./reducers/auth.slice";
 import { useAppDispatch, useAppSelector } from "./hooks/redux";
 import { Adjust } from "./components/adjust/adjust";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 function App() {
   const {adjustToken} = useAppSelector(state => state.auth)
@@ -29,6 +30,15 @@ function App() {
       <Box display="flex" alignItems="center" justifyContent="center">
         {adjustToken.length ? <Adjust/> : <Login setLoginCallback={loginCallback}/>}
       </Box>
+      <Link
+        className="github-link"
+        href="https://github.com/suharelli/adjust-extras"
+        target="_blank"
+        rel="noopener"
+        color="inherit"
+      >
+        <GitHubIcon className="github-link_icon"></GitHubIcon>
+      </Link>
     </>
   );
 }
